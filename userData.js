02f7943642c773620c4b762b8410a26bfd0fad2e
FreezePhoenix <24441367+FreezePhoenix@users.noreply.github.com>
2025-07-01 17:09:52 -0400

    Update userData.js

--- userData.js
+++ userData.js
@@ -1,6 +1,7 @@
 const SCRIPT = "BScorpionPriest.js"
-const SERVER = "US I";
-let RUN_MERCH = process.env.runmerch == "true";
+const SERVER = "US III";
+let RUN_MERCH = false;
+
 module.exports = {
     "config": {
         "fetch": false,
@@ -14,75 +15,82 @@ module.exports = {
         "email": process.env.username,
         "password": process.env.password
     },
-    "bots": [
-        {
-            "characterName": "AriaHarper",
-            "characterId": "5513766260178944",
-            "runScript": "BScorpAngyMerch.js",
-            "server": "US II"
-        }, // */
-        {
-            "characterName": "Rael",
-            "characterId": "6011174247202816",
-            "runScript": SCRIPT, 
-            "server": SERVER
-        }, // */
-        {
-            "characterName": "Geoffriel",
-            "characte rId": "5178085749030912",
-            "runScript": SCRIPT,
-            "server": SERVER
-        }, // */
-        {
-            "characterName": "Raelina",
-            "characterId": "5931316711784448",
-            "runScript": SCRIPT,
-            "server": RUN_MERCH ? "US II" : SERVER
-        }, // */
-        /* {
-            "characterName": "Anony",
-            "characterId": "5130368149094400",
-            "runScript": SCRIPT,
-            "server": SERVER
-        }, // */
-        /* {
-            "characterName": "Malthael",
-            "characterId": "5678553665896448",
-            "runScript": SCRIPT,
-            "server": SERVER
-        }, // */
-        /* {
-            "characterName": "Boismon",
-            "characterId": "5760997546524672",
-            "runScript": "DarkHound.js",
-            "server": "US I"
-        }, // */
-        /* {
-            "characterName": "Firenus",
-          "characterId": "6389196039127040",
-            "runScript": "Crab.js",
-            "server": "EU II"
-        }, // */
-        /* {
-            "characterName": "Malthael",
-            "characterId": "5678553665896448",
-            "runScript": "CrabZap.js",
-            "server": "US I"
-        }, // */
-        /* {
-            "characterName": "Daedulaus",
-            "characterId": "5826105459081216",
-            "runScript": "Crab.js",
-            "server": "EU II"
-        }, // */
-        /* {
-            "characterName": "Mage40",
-            "characterId": "5541032182153216",
-            "runScript": "default.js",
-            "server": "EU I"
-        } // */
-    ]
-}
-if(!RUN_MERCH) {
-  module.exports.bots.shift();
+    "toggleMerch": function() {
+      RUN_MERCH = !RUN_MERCH;
+    },
+    "bots": function() { 
+      let BOTS = [
+          {
+              "characterName": "AriaHarper",
+              "characterId": "5513766260178944",
+              "runScript": "BScorpAngyMerch.js",
+              "server": "US II"
+          }, // */
+          {
+              "characterName": "Rael",
+              "characterId": "6011174247202816",
+              "runScript": SCRIPT, 
+              "server": SERVER
+          }, // */
+          {
+              "characterName": "Geoffriel",
+              "characte rId": "5178085749030912",
+              "runScript": SCRIPT,
+              "server": SERVER
+          }, // */
+          {
+              "characterName": "Raelina",
+              "characterId": "5931316711784448",
+              "runScript": SCRIPT,
+              "server": RUN_MERCH ? "US II" : SERVER
+          }, // */
+          /* {
+              "characterName": "Anony",
+              "characterId": "5130368149094400",
+              "runScript": SCRIPT,
+              "server": SERVER
+          }, // */
+          /* {
+              "characterName": "Malthael",
+              "characterId": "5678553665896448",
+              "runScript": SCRIPT,
+              "server": SERVER
+          }, // */
+          /* {
+              "characterName": "Boismon",
+              "characterId": "5760997546524672",
+              "runScript": "DarkHound.js",
+              "server": "US I"
+          }, // */
+          /* {
+              "characterName": "Firenus",
+            "characterId": "6389196039127040",
+              "runScript": "Crab.js",
+              "server": "EU II"
+          }, // */
+          /* {
+              "characterName": "Malthael",
+              "characterId": "5678553665896448",
+              "runScript": "CrabZap.js",
+              "server": "US I"
+          }, // */
+          /* {
+              "characterName": "Daedulaus",
+              "characterId": "5826105459081216",
+              "runScript": "Crab.js",
+              "server": "EU II"
+          }, // */
+          /* {
+              "characterName": "Mage40",
+              "characterId": "5541032182153216",
+              "runScript": "default.js",
+              "server": "EU I"
+          } // */
+      ]
+      
+      if(!RUN_MERCH) {
+        BOTS.shift();
+      }
+      return BOTS;
+    }
 }
