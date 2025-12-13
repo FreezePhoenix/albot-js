31f67c0d3996a5fe8acd803f6f20792ffb6b1778
FreezePhoenix <24441367+FreezePhoenix@users.noreply.github.com>
2025-12-13 11:22:48 -0500

    Add character checks for Rael and Geoffriel

--- CODE/BScorpionPriest.js
+++ CODE/BScorpionPriest.js
@@ -3,6 +3,13 @@ if(character.name == "Raelina") {
 	parent.switch_script("hawk");
 }
 
+if(character.name == "Rael") {
+	parent.switch_script("dracul");
+}
+if(character.name == "Geoffriel") {
+	parent.switch_script("crab");
+}
+
 const {
 	Lazy,
 	Adapter,
