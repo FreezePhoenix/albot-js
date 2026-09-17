bb9893b1c11422c61b38c7131073fdc5dda657bb
FreezePhoenix <24441367+FreezePhoenix@users.noreply.github.com>
2026-09-16 22:56:37 -0400

    Implement stand analysis and restock functionality

--- CODE/BScorpionMerchant.js
+++ CODE/BScorpionMerchant.js
@@ -1,10 +1,177 @@
-
 function ms_until(skill_name, timestamp = new Date()) {
 	if (skill_name in parent.next_skill) {
 		return parent.next_skill[skill_name] - timestamp;
 	}
 	return -Infinity;
 };
+let stand_analysis = {};
+const analyze = () => {
+	const buy = (stand_analysis.buy = {});
+	const sell = (stand_analysis.sell = {});
+	let max = 4;
+	if (character.stand) {
+		max = 30;
+	}
+	for (let i = 0; i++ < max; i) {
+		let slot_id = "trade" + i;
+		let slot = character.slots[slot_id];
+		if (slot != null) {
+			if (slot.b === true) {
+				let analysis = (buy[slot.name] ??= []);
+				analysis.push([slot_id, slot.price, slot.q ?? 1, slot.level ?? 0])
+			} else {
+				let analysis = (sell[slot.name] ??= []);
+				analysis.push([slot_id, slot.price, slot.q ?? 1, slot.level ?? 0])
+			}
+		}
+	}
+};
+stand_analysis.analysis = stand_analysis;
+stand_analysis.analyze = analyze;
+
+const FALSE_STUB = () => false;
+
+function first_null_trade(max) {
+	for (let i = 0; i++ < max; ) {
+		let slot = character.slots['trade' + i];
+		if (slot == null) {
+			return i;
+		}
+	}
+	return -1;
+}
+
+let def = { buy: {}, sell: {} };
+/**
+ * Restockables takes the form of: { buy: { item_name: [gold, quantity, level] }, sell: { item_name: [gold, quantity, level, remove_filter, is_whitelist]}}
+ */
+function restock(restockables) {
+	def = restockables;
+}
+setInterval(() => {
+	let restockables = def;
+	let max = 4;
+	if (character.stand) {
+		max = 30;
+	}
+	stand_analysis.analyze();
+	for (let name in restockables.sell) {
+		let [gold, quantity, level, remove_filter = FALSE_STUB, is_whitelist = false] =
+			restockables.sell[name];
+		// Do we have this item on our stand?
+		let sell_analysis = stand_analysis.analysis.sell[name];
+		if (sell_analysis && quantity != -1) {
+			// We do! Where is it? How many do we have listed?
+			let [slot, cur_price, cur_quantity, cur_level] = sell_analysis[0];
+			// If we have less listed than we want to, see if we can fix that.
+			if (remove_filter() ^ is_whitelist) {
+				parent.socket.emit('unequip', {slot});
+				break;
+			}
+			if (cur_quantity < quantity) {
+				// We don't. But, we might have it in our inventory.
+				// Find the item. If we don't specify a level (-1 is put for a level) then match anything.
+				let index = -1;
+				let count = 0;
+				for (let i = 0; i < character.isize; i++) {
+					if (character.items[i]?.name === name) {
+						if (
+							level === -1 ||
+							character.items[i].level === level
+						) {
+							index = i;
+							count = character.items[i].q ?? 1;
+							break;
+						}
+					}
+				}
+
+				if (index !== -1) {
+					count += cur_quantity;
+					// If we have more of the item than the quantity we want listed, lower the amount to list to the max
+					if (count > quantity) {
+						count = quantity;
+					}
+					parent.socket.emit('unequip', { slot });
+					parent.socket.emit(
+						'equip',
+						{ q: count, slot, num: index, price: gold }
+					);
+
+					// Only list one item please!
+					break;
+				}
+			}
+		} else if (!(remove_filter() ^ is_whitelist)) {
+			// We don't. But, we might have it in our inventory.
+			// Find the item. If we don't specify a level (-1 is put for a level) then match anything.
+			let num = -1;
+			let count = 0;
+			for (let i = 0; i < 42; i++) {
+				if (character.items[i]?.name === name) {
+					if (level === -1 || character.items[i].level === level) {
+						num = i;
+						count = character.items[i].q ?? 1;
+						break;
+					}
+				}
+			}
+
+			if (num !== -1) {
+				const open_slot = first_null_trade(max);
+				if (open_slot !== -1) {
+					// If we have more of the item than the quantity we want listed, lower the amount to list to the max
+					if (count > quantity) {
+						count = quantity;
+					}
+					parent.socket.emit(
+						'equip',
+						{ q: count, slot: 'trade' + open_slot, num, price: gold }
+					);
+
+					// Only list one item please!
+					break;
+				}
+			}
+		}
+	}
+	for (let name in restockables.buy) {
+		let [price, count, level] = restockables.buy[name];
+		// Do we have this item on our stand?
+		let buy_analysis = stand_analysis.analysis.buy[name];
+		if (buy_analysis) {
+			// We do! Where is it? How many do we have listed?
+			let [slot, cur_price, cur_quantity, cur_level] = buy_analysis[0];
+			// If we have less listed than we want to, see if we can fix that.
+			if (cur_quantity < count) {
+				parent.socket.emit('unequip', { slot });
+				parent.socket.emit(
+					'trade_wishlist',
+					{ q: count, slot, price, level, name }
+				);
+				// Only list one item please!
+				break;
+			}
+		} else {
+			const open_slot = first_null_trade(max);
+			if (open_slot !== -1) {
+				parent.socket.emit(
+					'trade_wishlist',
+					{ q: count, slot: 'trade' + open_slot, price, level, name }
+				);
+			}
+		}
+	}
+}, 1000);
+
+restock({
+	sell: {
+		offeringp: [5000000, 500, -1],
+	},
+	buy: {},
+});
+
+
 (async () => {
 	function CompleteAdapter(...properties) {
 		return Function(
@@ -823,6 +990,7 @@ function ms_until(skill_name, timestamp = new Date()) {
 		'helmet',
 		'sstinger',
 		'beewings',
+		'hpot1',
 		'scroll0',
 		'scroll1',
 		'cscroll0',
@@ -1317,7 +1485,9 @@ function ms_until(skill_name, timestamp = new Date()) {
 		.level('9', '==')
 		.build();
 		const L_CAPE_FILTER = ItemFilter.ofName('ecape')
-		.level('9', '==').build();
+		.level('8', '==')
+		.property('festive')
+		.build();
 		const L_PANTS_FILTER = ItemFilter.ofName('wbreeches')
 		.level('6', '==')
 		.build();
@@ -1351,7 +1521,7 @@ function ms_until(skill_name, timestamp = new Date()) {
 			for(let x in parent.entities) {
 				let etarget = parent.entities[x];
 				if (
-					etarget.hp < 20000 &&
+					etarget.hp < 40000 &&
 					etarget.mtype == 'bscorpion'
 				) {
 					USING_LUCK = true;
@@ -1389,7 +1559,7 @@ function ms_until(skill_name, timestamp = new Date()) {
 			[D_RING_FILTER, 'ring1'],
 			[D_CAPE_FILTER, 'cape'],
 			[D_OFFHAND_FILTER, 'offhand'],
-			[D_SHIRT_FILTER, 'chest'],
+			[COAT_13, 'chest'],
 			[D_MAINHAND_FILTER, 'mainhand'],
 			[D_AMULET_FILTER, 'amulet'],
 			[D_EARRING_FILTER, 'earring1'],
